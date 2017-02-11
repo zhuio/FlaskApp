2353f48c4178e040018de7c5ffe8e940c0c09a94
zhuio <zhuiox@gmail.com>
2017-02-11 20:25:00 +0800

    astro

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return "Hi there, how about tonight?"
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
