4587d68e5339a1d0630eabf60a4d2c3fa9d393d3
zhuio <zhuiox@gmail.com>
2017-02-12 01:24:22 +0800

    new

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -6,6 +6,22 @@ app = Flask(__name__)
 def homepage():
     return render_template('index.html')
 
+@app.route('/dashboard/')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/signup/')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
+
 
 if __name__ == "__main__":
     app.run()
